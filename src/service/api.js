973404d77248857b1c9bfb1d300b4e3d80fbacc5
vunge7 <vunge7.1990@gmail.com>
2025-07-07 09:20:07 +0100

    Nova actualizacao

--- src/service/api.js
+++ src/service/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const ip = '10.84.211.78';
+//const ip = '10.84.211.78';
+const ip = '10.59.24.78';
 //const ip = '192.168.110.78';
 export const api = axios.create({
     baseURL: `https://${ip}:8081/`,
