8986ebb8f526e757f56be67b3036837ccc63be19
RobbieAlgon <robbiealgon@gmail.com>
2025-07-08 09:10:49 +0100

    Resolvendo Conflitos

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
-const ip = '10.84.211.78';
+//const ip = '10.84.211.78';
 //const ip = '192.168.110.78';
+const ip = 'localhost';
 export const api = axios.create({
-    baseURL: `https://${ip}:8081/`,
+    baseURL: `http://${ip}:8081/`,
 });
