755bca604e1894d353f9dbe37a3ee5d97e4a42a2
vunge7 <vunge7.1990@gmail.com>
2025-10-27 11:17:55 +0100

    Updated1

--- src/service/api.js
+++ src/service/api.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
 
+const ip = 'localhost:8081';
 
-const ip = 'api-ywq2.onrender.com';
+//const ip = 'api-ywq2.onrender.com';
 
 
 export const api = axios.create({
-    baseURL: `https://${ip}/`,
+    //baseURL: `https://${ip}/`,
+    baseURL: `http://${ip}/`,
     withCredentials: true, // Adiciona suporte a credenciais
 });
 
